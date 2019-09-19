3773d7ccdec73012ea5ccecdb0aa94f0545bc03e
Filip Bahnan <filipanton@hotmail.com>
2019-09-19 20:45:38 +0200

    Added some classes, tested spawning nodes

---
+++ VR-Graphical-Representation/Assets/Scripts/spawn_nodes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_nodes : MonoBehaviour
+{
+    public data softwareArchitecture;
+    public GameObject prefab;
+    public float nodeSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNodes();
+    }
+
+    void spawnNodes()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+            GameObject newObj = Instantiate(prefab, spawnPosition, Quaternion.identity);
+            newObj.transform.localScale += new Vector3(nodeSize, nodeSize, nodeSize);
+        }
+    }
+
+    void spawnLines()
+    {
+
+    }
+
+}
